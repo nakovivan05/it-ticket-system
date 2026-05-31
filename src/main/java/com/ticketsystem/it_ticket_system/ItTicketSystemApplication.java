c8d7ba9ac099548baf414f5f6ba0189b6e5522e3
Ivan Nakov <ivannakov11@gmail.com>
2026-05-31 11:03:23 +0300

    chore: initial project setup

---
+++ src/main/java/com/ticketsystem/it_ticket_system/ItTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticketsystem.it_ticket_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketsystem/it_ticket_system/ItTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketsystem.it_ticket_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
