be40ffdc5780ab783d799d4897f0dfbb88fdf152
Ivan Nakov <ivannakov11@gmail.com>
2026-06-02 15:27:59 +0300

    feat: create User, Category, Ticket entities

---
+++ src/main/java/com/ticketsystem/it_ticket_system/model/Ticket.java
@@ -0,0 +1,65 @@
+package com.ticketsystem.it_ticket_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at",nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "resolved_at",nullable = true)
+    private LocalDateTime resolvedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private TicketStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "reporter_id", nullable = false)
+    private User reporter;
+
+    @ManyToOne
+    @JoinColumn(name = "assignee_id", nullable = true)
+    private User assignee;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/ticketsystem/it_ticket_system/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.ticketsystem.it_ticket_system.model;
+
+public enum UserRole {
+    EMPLOYEE,
+    TECHNICIAN,
+    ADMIN
+}
