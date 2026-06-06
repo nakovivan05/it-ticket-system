57bf03fb7baadbd4a4fa92615de575276ebad36f
Ivan Nakov <ivannakov11@gmail.com>
2026-06-06 10:18:36 +0300

    feat: implement CategoryService and CategoryDTO and add custom CategoryNotFoundException

--- src/main/java/com/ticketsystem/it_ticket_system/dto/CategoryDTO.java
+++ src/main/java/com/ticketsystem/it_ticket_system/dto/CategoryDTO.java
@@ -1,4 +1,22 @@
 package com.ticketsystem.it_ticket_system.dto;
 
+import com.ticketsystem.it_ticket_system.model.Category;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+
+    public static CategoryDTO fromEntity(Category category){
+        return CategoryDTO.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .description(category.getDescription())
+                .build();
+    }
+
 }
--- src/main/java/com/ticketsystem/it_ticket_system/exception/UserNotFoundException.java
+++ src/main/java/com/ticketsystem/it_ticket_system/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.ticketsystem.it_ticket_system;
+package com.ticketsystem.it_ticket_system.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
