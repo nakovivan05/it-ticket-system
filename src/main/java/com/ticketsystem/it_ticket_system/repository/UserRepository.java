6f7ef62d15563c737138ba6c548f111acae33db2
Ivan Nakov <ivannakov11@gmail.com>
2026-06-09 16:28:49 +0300

    style: remove unused methods from TicketRepository and add two validation methods in UserRepository

--- src/main/java/com/ticketsystem/it_ticket_system/repository/UserRepository.java
+++ src/main/java/com/ticketsystem/it_ticket_system/repository/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     List<User> findByRole(UserRole role);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
