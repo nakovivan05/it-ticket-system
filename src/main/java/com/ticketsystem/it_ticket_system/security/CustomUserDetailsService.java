414972ffbf9e6fe55161459b094ad0d939513dc8
Ivan Nakov <ivannakov11@gmail.com>
2026-06-13 16:48:51 +0300

    feat: create CustomUserDetailsService for database authentication

--- src/main/java/com/ticketsystem/it_ticket_system/security/CustomUserDetailsService.java
+++ src/main/java/com/ticketsystem/it_ticket_system/security/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package com.ticketsystem.it_ticket_system.security;
+import com.ticketsystem.it_ticket_system.model.User;
+import com.ticketsystem.it_ticket_system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class CustomUserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username)
+                );
+
+        return user;
+    }
 }
