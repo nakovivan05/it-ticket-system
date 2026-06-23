9586cfbda5c2adf4d433a064d0900f52ff06fe8c
Ivan Nakov <ivannakov11@gmail.com>
2026-06-23 23:16:01 +0300

    feat: add CommentService, CommentDTO for request responses, UpdateCommentDTO for PUT requests, CreateCommentDTO for POST requests and custom exception CommentNotFoundException

--- src/main/java/com/ticketsystem/it_ticket_system/dto/CommentDTO.java
+++ src/main/java/com/ticketsystem/it_ticket_system/dto/CommentDTO.java
@@ -1,4 +1,35 @@
 package com.ticketsystem.it_ticket_system.dto;
 
+import com.ticketsystem.it_ticket_system.model.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDTO {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long ticketId;
+    private Long userId;
+
+    public static CommentDTO fromEntity(Comment comment)
+    {
+        return CommentDTO.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .ticketId(comment.getTicket().getId())
+                .userId(comment.getUser().getId())
+                .build();
+
+    }
 }
--- src/main/java/com/ticketsystem/it_ticket_system/service/CommentService.java
+++ src/main/java/com/ticketsystem/it_ticket_system/service/CommentService.java
@@ -1,4 +1,127 @@
 package com.ticketsystem.it_ticket_system.service;
 
+import com.ticketsystem.it_ticket_system.dto.CommentDTO;
+import com.ticketsystem.it_ticket_system.dto.CreateCommentDTO;
+import com.ticketsystem.it_ticket_system.dto.UpdateCommentDTO;
+import com.ticketsystem.it_ticket_system.exception.CommentNotFoundException;
+import com.ticketsystem.it_ticket_system.exception.TicketNotFoundException;
+import com.ticketsystem.it_ticket_system.exception.UserNotFoundException;
+import com.ticketsystem.it_ticket_system.model.Comment;
+import com.ticketsystem.it_ticket_system.model.Ticket;
+import com.ticketsystem.it_ticket_system.model.User;
+import com.ticketsystem.it_ticket_system.repository.CommentRepository;
+import com.ticketsystem.it_ticket_system.repository.TicketRepository;
+import com.ticketsystem.it_ticket_system.repository.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
+
+    public CommentService(CommentRepository commentRepository, TicketRepository ticketRepository, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<CommentDTO> getAllComments()
+    {
+        return commentRepository.findAll().stream()
+                .map(CommentDTO::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','EMPLOYEE','TECHNICIAN')")
+    public CommentDTO getCommentById(Long id)
+    {
+        String user = getCurrentUser();
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found with id: " + id));
+
+        return CommentDTO.fromEntity(comment);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'TECHNICIAN')")
+    public CommentDTO createComment(Long ticketId, CreateCommentDTO createCommentDTO)
+    {
+        Ticket ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with id: " + ticketId));
+        String user = getCurrentUser();
+        User currentUser = userRepository.findByUsername(user)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + user));
+        Comment comment = Comment.builder()
+                .content(createCommentDTO.getContent())
+                .ticket(ticket)
+                .user(currentUser)
+                .build();
+        Comment savedComment = commentRepository.save(comment);
+        return CommentDTO.fromEntity(savedComment);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'TECHNICIAN')")
+    public CommentDTO updateComment(Long ticketId, Long commentId, UpdateCommentDTO updateCommentDTO)
+    {
+        Ticket ticket = ticketRepository
+                .findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with id: " + ticketId));
+
+        Comment comment = commentRepository
+                .findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found with id: " + commentId));
+
+        if(!ticket.equals(comment.getTicket())) {
+            throw new SecurityException("Comment does not belong to this ticket");
+        }
+
+        String user = getCurrentUser();
+        User currentUser = userRepository.findByUsername(user)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + user));
+
+        if (!currentUser.equals(comment.getUser()) && !hasRole("ADMIN")) {
+            throw new SecurityException("User is not authorized to update this comment");
+        }
+        comment.setContent(updateCommentDTO.getContent());
+        Comment savedComment = commentRepository.save(comment);
+        return CommentDTO.fromEntity(savedComment);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'TECHNICIAN')")
+    public void deleteComment(Long commentId)
+    {
+        Comment comment = commentRepository
+                .findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found with id: " + commentId));
+
+        String user = getCurrentUser();
+        User currentUser = userRepository.findByUsername(user)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + user));
+        if (!currentUser.equals(comment.getUser()) && !hasRole("ADMIN")) {
+            throw new SecurityException("User is not authorized to delete this comment");
+        }
+        commentRepository.delete(comment);
+    }
+
+    public String getCurrentUser()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return username;
+    }
+
+    public Boolean hasRole(String role)
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_" + role));
+    }
+
 }
