e7ac5d11812281e99e8a28175cfb7ad32a2d02b5
Ivan Nakov <ivannakov11@gmail.com>
2026-08-14 22:24:08 +0300

    feat: add OpenAPI dependency to use SwaggerUI and test endpoints

--- src/main/java/com/ticketsystem/it_ticket_system/config/OpenAPIConfig.java
+++ src/main/java/com/ticketsystem/it_ticket_system/config/OpenAPIConfig.java
@@ -1,4 +1,27 @@
 package com.ticketsystem.it_ticket_system.config;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IT Ticket System API")
+                        .version("1.0")
+                        .description("Enterprise IT Ticket Management System")
+                )
+        .components(new Components()
+                .addSecuritySchemes("jwt", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
 }
